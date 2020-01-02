cdb6f4bf0bc6931acb9146a749151bfefc91d8f8
lercat <ddbieres@gmail.com>
2020-01-02 11:22:36 +0100

    PostsController

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,10 @@
             <img src="../assets/img/cheers.jpg" class="rounded-circle" alt="cheers">
         </div>
         <div class="col-9 pt-5">
-            <div> <h1> {{ $user->username }}</h1></div>
+            <div class="d-flex justify-content-between align-items-baseline"> 
+                <h1> {{ $user->username }}</h1>
+                <a href="#">Ajouter un nouveau Post</a>
+            </div>
             <div class="d-flex">
                 <div class="pr-5"><strong>153</strong> posts</div>
                 <div class="pr-5"><strong>23k</strong> followers</div>
