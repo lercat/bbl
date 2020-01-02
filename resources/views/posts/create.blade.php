8e653c47da9a88d7c5f8ec42803e179755ef61ee
lercat <ddbieres@gmail.com>
2020-01-02 16:07:03 +0100

    create post form

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,21 +2,56 @@
 
 @section('content')
 <div class="container">
-   
-<div class="row">
-    <div class="col-8 offset-2">
-        <div class="form-group row">
-            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-                <div class="col-md-6">
-                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-                    @error('name')
-                    <span class="invalid-feedback" role="alert">
-                    <strong>{{ $message }}</strong>
-                    </span>
+   <form action="/p" enctype="multipart/form-data" method="post">
+        <div class="row">
+            <div class="col-8 offset-2">
+
+                <div class="row">
+                    <h1>Ajoutez un Post</h1>
+                </div>
+                <div class="form-group row">
+                    <label for="legende" class="col-md-4 col-form-label">Légende du post</label>
+                            
+                    <input id="legende" 
+                        type="text" 
+                        class="form-control @error('legende') is-invalid @enderror" 
+                        legende="legende" 
+                        value="{{ old('legende') }}" 
+                        required autocomplete="legende" autofocus>
+                        
+                        @error('legende')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                </div>  
+
+                <div class="row">
+                    <label for="description" class="col-md-4 col-form-label">Description du post</label>
+                    <input type="text" class="form-control-file" id="description" name="description">
+                    @error('description')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
                     @enderror
                 </div>
-        </div>  
-    </div>
-</div>
+
+                <div class="row">
+                    <label for="image" class="col-md-4 col-form-label">Image du post</label>
+                    <input type="file" class="form-control-file" id="image" name="image">
+                    @error('image')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                    @enderror
+                </div>
+
+                <div class="row pt-4">
+                    <button class="btn btn-primary">Ajoutez un nouveau Post
+                    </button>
+                </div>
+            </div>
+        </div>
+   </form>
 </div>
 @endsection
