28a3f31bbe3c5a858fe7ee96cdb1ecb4e0489ab6
lercat <ddbieres@gmail.com>
2020-02-09 20:05:42 +0100

    image du profil

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row">
         <div class="col-3 p-5">
-            <img src="../assets/img/cheers.jpg" class="rounded-circle" alt="cheers">
+            <img src="/storage/{{ $user->profile->image }}" class="rounded-circle w-100" alt="cheers">
         </div>
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline"> 
