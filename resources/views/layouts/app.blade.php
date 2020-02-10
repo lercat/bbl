dd7b22657bc3cee2abafdd8f05805c3ea8edda86
lercat <ddbieres@gmail.com>
2020-02-10 23:48:06 +0100

    follow and image profil

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand d-flex" href="{{ url('/') }}">
-                    <div> <img src="assets/img/logo.png" style="height:25px; border-right: 1px solid #333" class="pr-3" alt="logo"></div>
+                    <div> <img src="../assets/img/logo.png" style="height:25px; border-right: 1px solid #333" class="pr-3" alt="logo"></div>
                     <div class="pl-3 pt-1"> BeerBlog</div> 
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,13 +4,33 @@
 <div class="container">
    <div class="row">
       <div class="col-8">
-         <img src="/storage/{{ $post->image}}" class="w-100">
+         <img src="/storage/{{ $post->image }}" class="w-100">
       </div>
       <div class="col-4">
          <div>
-            <h3>{{ $post->user->username }}</h3>
+            <div class="d-flex align-items-center">
+               <div class="pr-3">
+                  <img src="{{ $post->user->profile->profileImage() }}" class="rounded-circle w-100" style="max-width: 40px">        
+               </div>
+               <div>
+                  <div class="font-weight-bold"> 
+                     <a href="/profile/{{ $post->user->id }}"> 
+                        <span class="text-dark">{{ $post->user->username }}</span>
+                     </a> |
+                     <a href="#" class="pl-3">Follow</a>
+                  </div>
+               </div>
+            </div>
 
-            <h4>{{ $post->legende }}</h4>
+            <hr>
+
+            <p>
+               <span class="font-weight-bold"> 
+                  <a href="/profile/{{ $post->user->id }}">
+                     <span class="text-dark" >{{ $post->user->username }}</span>
+                  </a>
+               </span>{{ $post->legende }}
+            </p>
             <p>{{ $post->description }}</p>
          </div>
       </div>
